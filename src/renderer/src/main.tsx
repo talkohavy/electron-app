b775e06c903beab0b9cf44d766b640a07a428ec5
Tal Kohavy <talkohavy@gmail.com>
2026-07-25 08:19:18 +0300

    css moved to common/styles

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 import App from './components/App';
-import './assets/main.css';
-import './index.css';
 import DarkThemeProvider from './providers/DarkThemeProvider';
+import './common/styles/main.css';
+import './common/styles/toast.css';
+import './index.css';
 
 // eslint-disable-next-line
 function Client() {
