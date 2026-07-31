7262084161959e9dd5d2c6dd64ff4b7ecd60c366
Tal Kohavy <talkohavy@gmail.com>
2026-07-31 19:02:19 +0300

    bring resources inside src

--- src/main/core/create-window/create-window.service.ts
+++ src/main/core/create-window/create-window.service.ts
@@ -1,9 +1,9 @@
 import { join } from 'path';
 import { is } from '@electron-toolkit/utils';
 import { WindowEvents } from '@root/common/constants';
-import { BrowserWindow, shell } from 'electron';
 // @ts-ignore - handled by electron-vite's ?asset loader
-import icon from '../../../../resources/icon.png?asset';
+import icon from '@root/resources/icon.png?asset';
+import { BrowserWindow, shell } from 'electron';
 
 /**
  * Creates the main application window and wires its lifecycle. Extracted from
